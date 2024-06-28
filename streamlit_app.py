ec5d9db83322607454946f4ca3b4fe6e719bedd2
yulli-pku <illuy127@gmail.com>
2024-06-28 11:38:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import pandas as pd 
 import requests
 from st_aggrid import AgGrid
-
 house=pd.read_csv("https://raw.githubusercontent.com/yulli-pku/yulli-pku/main/house_clean.csv")
 
 def main() : 
