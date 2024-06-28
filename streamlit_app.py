aaae415822b2868bbd860fdb3dc6111fd51f4ea0
yulli-pku <illuy127@gmail.com>
2024-06-28 14:12:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,9 +76,9 @@ def main() :
   st.write("Outside the form")
 
 #tab
-tab1,tab2=st.tabs("Tab 1","Tab 2")
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
+  tab1,tab2=st.tabs("Tab 1","Tab 2")
+  tab1.write("this is tab 1")
+  tab2.write("this is tab 2")
 if __name__ == '__main__' : 
   main()
 
