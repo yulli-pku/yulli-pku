e85f37afdb04c00c59c3c16afd9f9ed68004a53a
yulli-pku <illuy127@gmail.com>
2024-06-28 15:55:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ from st_aggrid import AgGrid
 house=pd.read_csv("https://raw.githubusercontent.com/yulli-pku/yulli-pku/main/house_clean.csv")
 
 def main() : 
-  with st.form("Input variable prediktif"):
+  st.header('Input Variable Prediktif')
+  with st.form("Inputvar"):
     icol1,icol2,icol3=st.columns(3)
     
     with icol1:
