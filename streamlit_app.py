c73af7ab74dc68e0e389cd05c0b1b29b012b87f5
yulli-pku <illuy127@gmail.com>
2024-06-28 14:01:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,8 @@ with st.form("Data Diri"):
   submitted = st.form_submit_button("Submit")
   if submitted:
     st.write("slider", slider_val, "checkbox", checkbox_val)
-    st.write("Outside the form")
+  
+  st.write("Outside the form")
   
   if __name__ == '__main__' : 
   main()
