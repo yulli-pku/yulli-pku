2252bdc5c23163883717d61e0637419c0d46425e
yulli-pku <illuy127@gmail.com>
2024-06-28 11:05:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st 
-
+import pandas as pd 
+import requests
+from st_aggrid import AgGrid
 def main() : 
   st.write('Minimal Example')
 
@@ -8,7 +10,18 @@ def main() :
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
-  
+
+house=pd.read_csv()
+
+ st.write('Contoh dataframe')
+ st.dataframe(titanic)
+ st.write('Contoh JSON')
+ st.json(flight_passanger_api)
+ st.write('Metrics')
+ st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+ st.write('Menampilkan Dataframe dengan St AgGrid')
+ AgGrid(titanic)
+ st.table([x for x in range(1,5)])
 if __name__ == '__main__' : 
   main()
 
