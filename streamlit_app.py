b28de535982e590fde0c777c7a0a00fef8698df9
yulli-pku <illuy127@gmail.com>
2024-06-28 11:08:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,17 +11,17 @@ def main() :
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
 
- house=pd.read_csv("https://raw.githubusercontent.com/yulli-pku/yulli-pku/main/house_clean.csv")
+  house=pd.read_csv("https://raw.githubusercontent.com/yulli-pku/yulli-pku/main/house_clean.csv")
 
- st.write('Contoh dataframe')
- st.dataframe(house)
- st.write('Contoh JSON')
- st.json(flight_passanger_api)
- st.write('Metrics')
- st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
- st.write('Menampilkan Dataframe dengan St AgGrid')
- AgGrid(titanic)
- st.table([x for x in range(1,5)])
+  st.write('Contoh dataframe')
+  st.dataframe(house)
+  st.write('Contoh JSON')
+  st.json(flight_passanger_api)
+  st.write('Metrics')
+  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  AgGrid(titanic)
+  st.table([x for x in range(1,5)])
 if __name__ == '__main__' : 
   main()
 
