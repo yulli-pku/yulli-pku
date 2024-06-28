9b2e008b571c131befed5461311c1b688e718f69
yulli-pku <illuy127@gmail.com>
2024-06-28 10:02:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,12 @@ import streamlit as st
 def main() : 
   st.write('Minimal Example')
 
+  st.header('This is Header')
+  st.subheader('This is SubHeader')
+  st.markdown('# Rendering Markdown ')
+  st.write('Some Phytagorean Equation : ')
+  st.latex('c^2 = a^2+b^2')
+  
 if __name__ == '__main__' : 
   main()
 
