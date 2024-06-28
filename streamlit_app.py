224e112741b0d2f26615d76f3227cda3159829ae
yulli-pku <illuy127@gmail.com>
2024-06-28 11:11:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def main() :
   #st.write('Metrics')
   #st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
   #st.write('Menampilkan Dataframe dengan St AgGrid')
-  AgGrid(house)
+  #AgGrid(house)
   st.table([x for x in range(1,5)])
 if __name__ == '__main__' : 
   main()
