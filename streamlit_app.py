58935c7d62ccce30dbd9812409842fc4a6e3f222
yulli-pku <illuy127@gmail.com>
2024-06-28 13:49:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,6 +38,31 @@ def main() :
   num_input = st.number_input('Input Berapapun')
   st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
 
+#sidebar 
+  sidebar_checkbox = st.sidebar.checkbox('Checkbox di Sidebar')
+  sidebar_radio_button = st.sidebar.radio('Pilih Menu',options=['A','B','C'])
+    
+  #columns :
+  col1, col2, col3 = st.columns(3)
+
+  with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+    #atau dengan assignment 
+    image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
+
+  with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+  with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
+    #expander 
+    #dengan with atau dengan assignment 
+  expander = st.expander("Klik Untuk Detail ")
+  expander.write('Anda Telah Membuka Detail')
+
 if __name__ == '__main__' : 
   main()
 
