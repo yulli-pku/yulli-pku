7d31096cddc004a738919e92153809107fa049a7
yulli-pku <illuy127@gmail.com>
2024-06-28 15:51:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,7 @@ house=pd.read_csv("https://raw.githubusercontent.com/yulli-pku/yulli-pku/main/ho
 def main() : 
   with st.form("Input variable prediktif"):
     icol1,icol2,icol3=st.columns(3)
-    
-    with icol1:
-      idata1=st.text_input("input nilai data1")
-    with icol2:
-      idata2=st.text_input("input nilai data2")
-    with icol3:
-      idata3=st.text_input("input nilai data3")
-      
-    klikresult = st.button('submit')
-    if klikresult :
-      st.success(idata2)
-
+ 
   
   st.write('Minimal Example')
 
