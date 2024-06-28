09fed75d09765e69e0d5318c8cfe3cd28f8dc1d9
yulli-pku <illuy127@gmail.com>
2024-06-28 14:34:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,11 +98,7 @@ def main() :
   st.success("Success message")
   st.line_chart(np.random.randn(30, 3))
   
-  with st.chat_message("user"):
-    st.write("Hello 👋")
-    # Display a chat input widget at the bottom of the app.
-    st.chat_input("Say something")
-    # Display a chat input widget inline.
+  
     
   with st.container():
     st.chat_input("Say something")
