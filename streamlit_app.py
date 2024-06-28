fad353bc0f1bfddb271748f852bd62b34eeaa3c7
yulli-pku <illuy127@gmail.com>
2024-06-28 10:05:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ def main() :
   st.subheader('This is SubHeader')
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
-  st.write('c^2 = a^2+b^2')
+  st.latex('c^2 = a^2+b^2')
   
 if __name__ == '__main__' : 
   main()
