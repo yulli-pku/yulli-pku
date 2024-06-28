8713ed498022651eb6a161898aa42aef4738f00f
yulli-pku <illuy127@gmail.com>
2024-06-28 14:04:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,11 +69,11 @@ def main() :
     checkbox_val = st.checkbox("Form checkbox")
 
   # Every form must have a submit button.
-  submitted = st.form_submit_button("Submit")
-  if submitted:
-    st.write("slider", slider_val, "checkbox", checkbox_val)
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+      st.write("slider", slider_val, "checkbox", checkbox_val)
   
-  st.write("Outside the form")
+    st.write("Outside the form")
   
 if __name__ == '__main__' : 
   main()
