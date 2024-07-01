de99b0f3d2a19027612b98e457c8320ccd23521a
yulli-pku <illuy127@gmail.com>
2024-07-01 09:59:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,21 @@
-#import library needed
-import pandas as pd
-import streamlit as st 
 import pickle
-import numpy as np
+import streamlit as st
 
-# Load the best model
-#with open('iris_model.pkl', 'rb') as f:
-#    model = pickle.load(f)
-model = pickle.load(open('iris_model.pkl', 'rb'))
+model = pickle.load(open('estimasi_mobil.sav', 'rb'))
 
-species = ['setosa', 'versicolor', 'virginica']
+st.title('Estimasi Harga Mobil Bekas')
 
-st.title("Iris Flower Classification")
-st.write("This app correctly classifies iris flower among 3 possible species")
+year = st.number_input('Input Tahun Mobil')
+mileage = st.number_input('Input Km Mobil')
+tax = st.number_input('Input Pajak Mobil')
+mpg = st.number_input('Input Konsumsi BBM Mobil')
+engineSize = st.number_input('Input Engine Size')
 
-# Creating Sidebar for inputs
-st.sidebar.title("Inputs")
-sepal_length = st.sidebar.number_input("sepal length (cm)", 4.3, 7.9, 5.0)
-sepal_width = st.sidebar.number_input("sepal width (cm)", 2.0, 4.4, 3.6)
-petal_length = st.sidebar.number_input("petal length (cm)", 1.0, 6.9, 1.4)
-petal_width = st.sidebar.number_input("petal width (cm)", 0.1, 2.5, 0.2)
+predict = ''
 
-# Button to trigger prediction
-if st.button("Predict"):
-# Getting Prediction from model
-    inp = np.array([sepal_length, sepal_width, petal_length, petal_width])
-    inp = np.expand_dims(inp, axis=0)
-    prediction = model.predict(inp)
-
-# Show Results when the button is clicked
-    result = species[np.argmax(prediction)]
-    st.write("**This flower belongs to " + result + " class**")
-
+if st.button('Estimasi Harga'):
+    predict = model.predict(
+        [[year, mileage, tax, mpg, engineSize]]
+    )
+    st.write ('Estimasi harga mobil bekas dalam Ponds : ', predict)
+    st.write ('Estimasi harga mobil bekas dalam IDR (Juta) :', predict*19000)
