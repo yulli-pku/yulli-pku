2f04a946ae58001339dd164a67689bd023c039ac
yulli-pku <illuy127@gmail.com>
2024-07-01 13:02:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,22 @@ import streamlit as st
 model = pickle.load(open('knn_model.pkl', 'rb'))
 
 st.title('Cek Air Quality')
+sidebar_header=st.sidebar.subheader('Sidebar menu')
 
-AQI= st.number_input('Input AQI')
-PM10= st.number_input('Input PM10')
-PM2_5= st.number_input('Input PM2_5')	
-NO2= st.number_input('Input NO2')
-SO2= st.number_input('Input SO2')	
-O3= st.number_input('Input O3')
-Temperature= st.number_input('Input Temperature')
-Humidity= st.number_input('Input Humidity')	
-WindSpeed= st.number_input('Input WindSpeed')
-RespiratoryCases= st.number_input('Input RespiratoryCases')
-CardiovascularCases	= st.number_input('Input CardiovascularCases')
-HospitalAdmissions= st.number_input('Input HospitalAdmissions')
-HealthImpactClass= st.number_input('Input HealthImpactClass')
+with sidebar_header:
+    AQI= st.number_input('Input AQI')
+    PM10= st.number_input('Input PM10')
+    PM2_5= st.number_input('Input PM2_5')	
+    NO2= st.number_input('Input NO2')
+    SO2= st.number_input('Input SO2')	
+    O3= st.number_input('Input O3')
+    Temperature= st.number_input('Input Temperature')
+    Humidity= st.number_input('Input Humidity')	
+    WindSpeed= st.number_input('Input WindSpeed')
+    RespiratoryCases= st.number_input('Input RespiratoryCases')
+    CardiovascularCases	= st.number_input('Input CardiovascularCases')
+    HospitalAdmissions= st.number_input('Input HospitalAdmissions')
+    HealthImpactClass= st.number_input('Input HealthImpactClass')
 
 predict = ''
 
